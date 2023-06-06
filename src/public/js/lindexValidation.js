36d4b67a01624b86daa9c62b081faf58e64cc416
GeekIronic <juanpablomahechap@gmail.com>
2023-06-05 22:08:25 -0500

    ama de Desarrollo para ver mergen

--- src/public/js/lindexValidation.js
+++ src/public/js/lindexValidation.js
@@ -1,3 +1,5 @@
 const TipoId = document.getElementById('txt_TypeId');
 const Identy = document.getElementById('txt_Ident');
 const form = document.getElementById('form-login');
+
+console.log('pruebas')
